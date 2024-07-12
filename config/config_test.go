83d1b8199961d5ad3a70e2e710389e7a9ab184e7
Jay <jay@Jays-MacBook-Pro.local>
2024-07-11 21:15:28 -0700

    add some tests for configs

--- config/config_test.go
+++ config/config_test.go
@@ -1,106 +1,44 @@
 package config
 
-//
-//import (
-//	"github.com/spf13/viper"
-//	"github.com/stretchr/testify/assert"
-//	"os"
-//	"testing"
-//)
-//
-//func TestLoadConfig(t *testing.T) {
-//	// Set the environment variable for the test
-//	err := os.Setenv("APP_ENV", "test")
+import (
+	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadConfig(t *testing.T) {
+	viper.AutomaticEnv()
+
+	config, err := LoadConfig("config.yaml")
+	assert.NoError(t, err)
+	assert.NotNil(t, config)
+	assert.Equal(t, int32(100), config.Frequency)
+	// HttpServer
+	assert.Equal(t, 8088, config.HttpServer.Port)
+	assert.Equal(t, "debug", config.HttpServer.Mode)
+	assert.Equal(t, "/", config.HttpServer.ContextPath)
+	// Storage
+	assert.Equal(t, "localhost:6379", config.Storage.RedisHost)
+	assert.Equal(t, 0, config.Storage.RedisDb)
+	assert.Equal(t, "schedule_chanel", config.Storage.Chanel)
+	assert.Equal(t, "scheduled_tasks", config.Storage.SchedulerKeyName)
+	// Kafka
+	assert.Equal(t, true, config.Kafka.Enabled)
+	assert.Equal(t, "localhost:9092", config.Kafka.Brokers)
+	assert.Equal(t, "scheduler", config.Kafka.Group)
+	assert.Equal(t, "Scheduler", config.Kafka.SchedulerTopic)
+	assert.Equal(t, "TaskExecution", config.Kafka.TaskExecutionTopic)
+}
+
+//func TestLoadConfigFromEnv(t *testing.T) {
+//	err := os.Setenv("REDIS_HOST", "XXX")
 //	assert.NoError(t, err)
 //
-//	// Create a temporary config file for testing
-//	configContent := `
-//httpServer:
-//  port: 8088
-//  mode: test
+//	viper.AutomaticEnv()
 //
-//storage:
-//  redisHost: localhost:6379
-//  redisPass:
-//  redisDb: 0
-//  chanel: schedule_chanel
-//  schedulerKeyName: scheduled_tasks
-//
-//kafka:
-//  enabled: true
-//  brokers: localhost:9092
-//  group: scheduler
-//  schedulerTopic: Scheduler
-//  taskExecutionTopic: TaskExecution
-//`
-//	configFile, err := os.CreateTemp("config", "config.test.yaml")
-//	assert.NotNil(t, configFile)
+//	config, err := LoadConfig("config.yaml")
 //	assert.NoError(t, err)
-//	defer func(name string) {
-//		e := os.Remove(name)
-//		assert.NoError(t, e)
-//	}(configFile.Name())
-//
-//	_, err = configFile.Write([]byte(configContent))
-//	assert.NoError(t, err)
-//
-//	// Set Viper to read the temporary config file
-//	viper.SetConfigFile(configFile.Name())
-//
-//	config, err := GetConfig()
-//	assert.NoError(t, err)
-//
-//	assert.Equal(t, 8080, config.HttpServer.Port)
-//	assert.Equal(t, "test", config.HttpServer.Mode)
-//	assert.Equal(t, "scheduled_tasks", config.Storage.SchedulerKeyName)
-//	assert.Equal(t, "schedule_chanel", config.Storage.Chanel)
-//	assert.Equal(t, 0, config.Storage.RedisDb)
-//}
-//
-//func TestLoadConfigWithEnvOverride(t *testing.T) {
-//	// Set the environment variables for the test
-//	os.Setenv("APP_ENV", "test")
-//	os.Setenv("APP_DATABASE_USER", "envuser")
-//	os.Setenv("APP_DATABASE_PASSWORD", "envpassword")
-//	os.Setenv("APP_DATABASE_HOST", "envhost")
-//	os.Setenv("APP_DATABASE_NAME", "envdatabase")
-//
-//	// Create a temporary config file for testing
-//	configContent := `
-//httpServer:
-//  port: 8088
-//  mode: test
-//
-//storage:
-//  redisHost: localhost:6379
-//  redisPass:
-//  redisDb: 0
-//  chanel: schedule_chanel
-//  schedulerKeyName: scheduled_tasks
-//
-//kafka:
-//  enabled: true
-//  brokers: localhost:9092
-//  group: scheduler
-//  schedulerTopic: Scheduler
-//  taskExecutionTopic: TaskExecution
-//`
-//	configFile, err := os.CreateTemp("", "config.test.yaml")
-//	assert.NoError(t, err)
-//	defer os.Remove(configFile.Name())
-//
-//	_, err = configFile.Write([]byte(configContent))
-//	assert.NoError(t, err)
-//
-//	// Set Viper to read the temporary config file
-//	viper.SetConfigFile(configFile.Name())
-//
-//	config, err := GetConfig()
-//	assert.NoError(t, err)
-//
-//	assert.Equal(t, 8080, config.HttpServer.Port)
-//	assert.Equal(t, "test", config.HttpServer.Mode)
-//	assert.Equal(t, "scheduled_tasks", config.Storage.SchedulerKeyName)
-//	assert.Equal(t, "schedule_chanel", config.Storage.Chanel)
-//	assert.Equal(t, 0, config.Storage.RedisDb)
+//	assert.NotNil(t, config)
+//	assert.Equal(t, "XXX", config.Storage.RedisPass)
 //}
